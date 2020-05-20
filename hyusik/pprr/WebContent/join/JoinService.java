2efffaf81cae39dcc39f108011b59ba81044a75c
Damien-Lee <leewon21gg@gmail.com>
2020-05-20 18:26:44 +0900

    회원가입 서비스/핸들러
    회원가입 
    회원가입서비스 핸들러

---
+++ hyusik/pprr/WebContent/join/JoinService.java
@@ -0,0 +1,46 @@
+package member.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+import jdbc.JdbcUtil;
+import jdbc.connection.ConnectionProvider;
+import member.dao.MemberDao;
+import member.model.Member;
+
+public class JoinService {
+
+	private MemberDAO memberDao = new MemberDAO();
+	
+	public void join(JoinRequest joinReq) {
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			
+			Member member = MemberDAO.selectById(conn, joinReq.getId());
+			if (member != null) {
+				JdbcUtil.rollback(conn);
+				throw new DuplicateIdException();
+			}
+			
+			MemberDAO.insert(conn, 
+					new Member(
+							joinReq.getId(), 
+							joinReq.getName(), 
+							joinReq.getPassword(), 
+							joinReq.getConfirmPassword(),
+							joinReq.getBirth(),
+							joinReq.getGender(),
+							joinReq.getAdmin()
+					);
+			conn.commit();
+		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
+	}
+}
